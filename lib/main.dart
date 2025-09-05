d87f5cbf385e9909f59e51dd79f26690ea3adeff
Thai2k25 <tha306471@gmail.com>
2025-09-05 19:32:28 +0700

    First commit - Flutter project

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_application_2/todo_module/todo_provider.dart';
+
+
+void main() {
+  //runApp(Container(color: Colors.yellow));
+  runApp(TodoProvider());
+}
---
+++ lib/todo_module/todo_provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/todo_module/todo_login_logic.dart';
+import 'package:flutter_application_2/todo_module/todo_splashscreen.dart';
+import 'package:provider/provider.dart';
+
+
+Widget TodoProvider() {
+  return MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (context) => TodoLoginLogic()),
+    ],
+    child:  TodoSplashScreen(),
+  );
+}
